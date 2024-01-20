207c0ef0d9704a1d30e0f4a338ade4727deb739f
ganbari <steinway.nathan@gmail.com>
2024-01-20 12:21:17 -0600

    begins redesign

---
+++ src/app/champ-select/champ-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-champ-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './champ-select.component.html',
+  styleUrl: './champ-select.component.css'
+})
+export class ChampSelectComponent {
+
+}
