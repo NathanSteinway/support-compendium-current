00e95e22288164f38f6431dc1738255ded8c3c1b
ganbari <steinway.nathan@gmail.com>
2024-01-23 14:40:54 -0600

    nearly completes champ page basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { ChampSelectComponent } from './champ-select/champ-select.component'
-
+import { ChampPageComponent } from './champ-page/champ-page.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ChampSelectComponent],
+  imports: [CommonModule, RouterOutlet, ChampSelectComponent, ChampPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
