2843957ae7ffb149074b79ba9e0459ac9612eb18
ganbari <steinway.nathan@gmail.com>
2024-02-26 11:05:02 -0600

    completes base functionality for spell icon and details delivery

--- src/app/champ-page-janna/champ-page-janna.component.ts
+++ src/app/champ-page-janna/champ-page-janna.component.ts
@@ -19,6 +19,15 @@ export class ChampPageJannaComponent {
   champName: any;
   spellType: any;
 
+  champId: number = 1
+  spellDetails: {spellId: string, spellIcon: string, spellName: string}[] = [
+    {spellId: 'passive', spellIcon: '../../assets/Janna_Tailwind.png', spellName: 'Tailwind'},
+    {spellId: 'q', spellIcon: '../../assets/Janna_Howling_Gale.png', spellName: 'Howling Gale'},
+    {spellId: 'w', spellIcon: '../../assets/Janna_Zephyr.png', spellName: 'Zephyr'},
+    {spellId: 'e', spellIcon: '../../assets/Janna_Eye_of_the_Storm.png', spellName: 'Eye of the Storm'},
+    {spellId: 'r', spellIcon: '../../assets/Janna_Monsoon.png', spellName: 'Monsoon'}
+  ]
+
   constructor(private champInfoService: ChampInfoService) {}
 
 
@@ -28,7 +37,7 @@ export class ChampPageJannaComponent {
     this.headers = [];
   }
 
-  showChampInfo(champId: number, spellId: string) {
+  showChampInfo(champId: number, spellId: string, spellIcon: string) {
 
     this.champInfoService.getInfo()
       .subscribe(data => {
