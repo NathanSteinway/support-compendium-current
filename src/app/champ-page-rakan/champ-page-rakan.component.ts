45bebb7e5667bfc3b4fd032ffaf28eb2ee9108a6
ganbari <steinway.nathan@gmail.com>
2024-02-27 14:33:31 -0600

    service adapted to rakan page

--- src/app/champ-page-rakan/champ-page-rakan.component.ts
+++ src/app/champ-page-rakan/champ-page-rakan.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Info, ChampInfoService } from '../champ-info.service';
 
 @Component({
   selector: 'app-champ-page-rakan',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './champ-page-rakan.component.html',
   styleUrl: './champ-page-rakan.component.css'
 })
-export class ChampPageRakanComponent {
-  spellInfo(): void {
-    console.log("Hello world!")
+export class ChampPageRakanComponent implements OnInit{
+  constructor(private champInfoService: ChampInfoService) {}
+
+  ngOnInit(): void {
+    this.getChampInfo();
+  }
+
+  getChampInfo(): void {
+    this.champInfoService.getInfo()
+    .subscribe(data => {
+      
+      this.info = { ...data };
+      
+    });
+  }
+
+  info: Info | undefined;
+
+  champName: any;
+  spellType: any;
+  champId: number = 0
+  spellId: string = ''
+
+  spellDetails: {spellId: string, spellIcon: string, spellName: string}[] = [
+    {spellId: 'passive_1', spellIcon: '../../assets/Fey_Feathers.png', spellName: 'Fey Feathers'},
+    {spellId: 'passive_2', spellIcon: '../../assets/Lovers_Leap.png', spellName: "Lover's Leap"},
+    {spellId: 'q', spellIcon: '../../assets/Gleaming_Quill.png', spellName: 'Gleaming Quill'},
+    {spellId: 'w', spellIcon: '../../assets/Grand_Entrance.png', spellName: 'Grand Entrance'},
+    {spellId: 'e', spellIcon: '../../assets/Battle_Dance.png', spellName: 'Battle Dance'},
+    {spellId: 'r', spellIcon: '../../assets/The_Quickness.png', spellName: 'The Quickness'}
+  ]
+
+  showChampInfo(champId: number, spellId: string) {
+    let champion = this.info?.champions[champId]
+    this.champName = champion
+    this.spellType = champion?.spells[spellId]
+    console.log(this.spellType)
   }
 }
