957d20781c30d20e615fda29f36c6ffd72642fb1
ganbari <steinway.nathan@gmail.com>
2024-04-02 14:37:29 -0500

    adds support items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterLink } from '@angular/router';
 import { NavComponent } from './nav/nav.component'
 import { HomepageWrapperComponent } from './homepage-wrapper/homepage-wrapper.component'
 import { VisionComponent } from './vision/vision.component'
+import { ItemsComponent } from './items/items.component'
 import { ChampPageRakanComponent } from './champ-page-rakan/champ-page-rakan.component'
 import { ChampPageJannaComponent} from './champ-page-janna/champ-page-janna.component'
 import { ChampPageMaokaiComponent } from './champ-page-maokai/champ-page-maokai.component'
@@ -13,7 +14,7 @@ import { ChampPageMaokaiComponent } from './champ-page-maokai/champ-page-maokai.
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, NavComponent, HomepageWrapperComponent, ChampPageRakanComponent, ChampPageJannaComponent, ChampPageMaokaiComponent, VisionComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavComponent, HomepageWrapperComponent, ChampPageRakanComponent, ChampPageJannaComponent, ChampPageMaokaiComponent, VisionComponent, ItemsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomepageWrapperComponent } from './homepage-wrapper/homepage-wrapper.component'
 import { VisionComponent } from './vision/vision.component'
+import { ItemsComponent } from './items/items.component'
 import { ChampPageRakanComponent } from './champ-page-rakan/champ-page-rakan.component';
 import { ChampPageJannaComponent } from './champ-page-janna/champ-page-janna.component';
 import { ChampPageMaokaiComponent } from './champ-page-maokai/champ-page-maokai.component';
@@ -8,6 +9,7 @@ import { ChampPageMaokaiComponent } from './champ-page-maokai/champ-page-maokai.
 export const routes: Routes = [
     {path: '', component: HomepageWrapperComponent },
     {path: 'vision-guide', component: VisionComponent},
+    {path: 'items-guide', component: ItemsComponent},
     {path: 'rakan-page', component: ChampPageRakanComponent},
     {path: 'janna-page', component: ChampPageJannaComponent},
     {path: 'maokai-page', component: ChampPageMaokaiComponent}
