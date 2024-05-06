cd74b7060613a923ca0c625b3adedc01160af9f7
ganbari <steinway.nathan@gmail.com>
2024-05-06 13:44:39 -0500

    added detail button slider

--- src/app/champ-page-rakan/champ-page-rakan.component.ts
+++ src/app/champ-page-rakan/champ-page-rakan.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChampInfoService } from '../services/champ-info.service';
 import { Info } from '../services/interfaces/champ-info.service.interface';
+import { DetailSliderComponent } from '../detail-slider/detail-slider.component'
 
 @Component({
   selector: 'app-champ-page-rakan',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DetailSliderComponent],
   templateUrl: './champ-page-rakan.component.html',
   styleUrl: './champ-page-rakan.component.css'
 })
+
 export class ChampPageRakanComponent implements OnInit{
 
   constructor(private champInfoService: ChampInfoService) {}
@@ -35,12 +37,12 @@ export class ChampPageRakanComponent implements OnInit{
   spellId: string = ''
 
   spellDetails: {spellId: string, spellIcon: string, spellName: string}[] = [
-    {spellId: 'passive_1', spellIcon: '../../assets/Fey_Feathers.png', spellName: 'Fey Feathers'},
-    {spellId: 'passive_2', spellIcon: '../../assets/Lovers_Leap.png', spellName: "Lover's Leap"},
-    {spellId: 'q', spellIcon: '../../assets/Gleaming_Quill.png', spellName: 'Gleaming Quill'},
-    {spellId: 'w', spellIcon: '../../assets/Grand_Entrance.png', spellName: 'Grand Entrance'},
-    {spellId: 'e', spellIcon: '../../assets/Battle_Dance.png', spellName: 'Battle Dance'},
-    {spellId: 'r', spellIcon: '../../assets/The_Quickness.png', spellName: 'The Quickness'}
+    {spellId: 'passive_1', spellIcon: '../../assets/spells/Fey_Feathers.png', spellName: 'Fey Feathers'},
+    {spellId: 'passive_2', spellIcon: '../../assets/spells/Lovers_Leap.png', spellName: "Lover's Leap"},
+    {spellId: 'q', spellIcon: '../../assets/spells/Gleaming_Quill.png', spellName: 'Gleaming Quill'},
+    {spellId: 'w', spellIcon: '../../assets/spells/Grand_Entrance.png', spellName: 'Grand Entrance'},
+    {spellId: 'e', spellIcon: '../../assets/spells/Battle_Dance.png', spellName: 'Battle Dance'},
+    {spellId: 'r', spellIcon: '../../assets/spells/The_Quickness.png', spellName: 'The Quickness'}
   ]
 
   showChampInfo(champId: number, spellId: string) {
